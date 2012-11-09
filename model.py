77dab78e1281ffc5c883ad2a40cc94f8b085dc86
Susan <onceuponatimeforever@gmail.com>
2012-11-08 16:31:35 -0800

    Sorry I accidentally deleted those files, Sonya

--- model.py
+++ model.py
@@ -2,44 +2,42 @@
 model.py
 """
 import sqlite3
-import datetime
+import datetime 
+
+TASK_COLS = ["id", "title", "created_at", "completed_at", "user_id"]
+USER_COLS = ["id", "email", "password", "username"]
 
 def connect_db():
     return sqlite3.connect("tipsy.db")
 
 def new_user(db, email, password, name):          
-    c = db.cursor()                                     
-    query = """INSERT INTO Users VALUES (NULL, ?, ?, ?)"""                                                           
-    c.execute(query, (email, password, name))           
-    db.commit()
+    vals= [email, password, name]
+    return insert_into_table(db, "Users", USER_COLS, vals)
 
+def new_task(db, title, user_id = None):
+    """Given a title and a user_id, create a new task belonging to that user. 
+    Return the id of the created task"""
+
+    vals= [title, datetime.datetime.now(), None, user_id ]
+    return insert_into_table(db, "Tasks", TASK_COLS, vals)
+
+def insert_into_table(db, table, columns, values):
+    c = db.cursor()
+    query_template = """INSERT into %s values (%s)"""
+    num_cols = len(columns)
+    q_marks = ", ".join(["NULL"] + (["?"] * (num_cols-1)))
+    query = query_template%(table, q_marks)
+    res = c.execute(query, tuple(values))
+    if res:
+        db.commit()
+        return res.lastrowid
+  
 def remove_user(db, user_id):          
     c = db.cursor()                                     
     query = """DELETE FROM Users WHERE id =?"""                                                           
     c.execute(query, (user_id,) )           
     db.commit()
     return "Deleted User: %d" %user_id 
-
-def authenticate(db, email, password):
-    c = db.cursor()
-    query = """SELECT * from Users WHERE email=? AND password=?"""
-    c.execute(query, (email, password))
-    result = c.fetchone()
-    if result:
-        fields = ["id", "email", "password", "username"]
-        return dict(zip(fields, result))
-
-    return None
-
-def new_task(db, title, user_id):
-    """Given a title and a user_id, create a new task belonging to 
-    that user. Return the id of the created task"""
-    time_stamp= str(datetime.datetime.now())
-    clean_time = time_stamp[:16]
-    c= db.cursor()
-    query = """INSERT INTO Tasks VALUES (NULL, ?, NULL, ?, ?) """
-    c.execute(query, (title, clean_time, user_id))
-    db.commit()
 
 def remove_task(db, task_id):          
     c = db.cursor()                                     
@@ -48,14 +46,69 @@ def remove_task(db, task_id):
     db.commit()
     return "Deleted Task: %d" %task_id 
 
-def get_user(db, user_id):  
-    """Gets a user dictionary out of the database given an id"""
+def authenticate(db, email, password):
     c = db.cursor()
-    query = """SELECT * from Users WHERE id=? """
-    c.execute(query, (user_id,))
-    result= c.fetchone()
-    fields = ["id", "email", "password", "name"] 
-    return dict(zip(fields, result))
+    query = """SELECT * from Users WHERE email=? AND password=?"""
+    c.execute(query, (email, password))
+    result = c.fetchone()
+    if result:
+        fields = ["id", "email", "password", "username"]
+        return make_user(result)
+    return None
+
+def make_user(row):
+    fields = ["id", "email", "password", "username"]
+    return dict(zip(fields, row))
+
+def make_task(row):
+    columns = ["id", "title", "created_at", "completed_at", 
+    "user_id"]
+    return dict(zip(columns, row))
+
+def get_user(db, user_id):
+    return get_from_table_by_id(db, "Users", user_id, make_user)
+
+def get_task(db, task_id):
+    return get_from_table_by_id(db, "Tasks", task_id, make_task)
+
+def get_from_table_by_id(db, tablename, id, make_dict_fn): 
+    c = db.cursor() 
+    query_template= """ SELECT * from %s WHERE id= ?""" 
+    query= query_template % tablename 
+
+    c.execute(query, (id,))
+    row= c.fetchone()
+    if row:
+        return make_dict_fn(row)
+    return None
+
+def complete_task(db, task_id):
+    """Mark the task with the given task_id as being complete."""
+    c = db.cursor()
+    query = """UPDATE Tasks SET completed_at=DATETIME('now') WHERE id=?"""
+    res = c.execute(query, (task_id,))
+    if res:
+        db.commit()
+        return res.lastrowid
+    else:
+        return None
+
+def get_tasks(db, user_id=None):
+    """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
+    c = db.cursor()
+    if user_id:
+        query = """SELECT * from Tasks WHERE user_id = ?"""
+        c.execute(query, (user_id,))
+    else:
+        query = """SELECT * from Tasks"""
+        c.execute(query)
+    tasks = []
+    rows = c.fetchall()
+    for row in rows:
+        task = make_task(row)
+        tasks.append(task)
+
+    return tasks
 
 def call_users(db, user_id=None):  
     """Pulls out the entire user database"""
@@ -76,57 +129,3 @@ def call_users(db, user_id=None):
                 row))
         users.append(user)
     return users
-
-def complete_task(db, task_id):
-    """Mark the task with the given task_id as being complete."""
-    c = db.cursor()
-    time_stamp= str(datetime.datetime.now())
-    clean_time = time_stamp[:16]
-    query = """UPDATE Tasks SET completed_at= ? WHERE id=?   """
-    c.execute(query, (clean_time, task_id,))
-    result= c.fetchone()
-    db.commit()
-    return True
-
-    # id INTEGER PRIMARY KEY,
-    # title VARCHAR(64),
-    # created_at DATETIME,
-    # completed_at DATETIME,
-    # user_id INTEGER
- 
-def get_tasks(db, user_id=None):
-    """Get all the tasks matching the user_id, getting all the 
-    tasks in the system if the user_id is not provided. 
-    Returns the results as a list of dictionaries."""
-
-    c = db.cursor()
-    
-    if user_id==None: 
-        query = """SELECT * from TASKS """
-        c.execute(query )  
-    else: 
-        query= """ SELECT * from TASKS WHERE id= ? """ 
-        c.execute(query, (user_id,))
-    
-    tasks=[] 
-    rows= c.fetchall()
-
-    for row in rows:
-        task = dict(zip(["id", "title", "created_at", "completed_at", "user_id"], 
-                row))
-        tasks.append(task)
-    return tasks
-
-def get_task(db, task_id):
-    """Gets a single task, given its id.
-    Returns a dictionary of the task data."""
-    c = db.cursor()
-     
-    query= """ SELECT * from TASKS WHERE id= ? """ 
-    c.execute(query, (task_id,))
-
-    result= c.fetchone()
-    fields = ["id", "title", "created_at", "completed_at", "user_id"] 
-    dict1= dict(zip(fields, result))
-    return result 
-
--- tipsy.py
+++ tipsy.py
@@ -1,75 +1,111 @@
 """
 tipsy.py -- A flask-based todo list
 """
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, request, session, g
 import model
+import datetime
 
 app = Flask(__name__)
+# SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.config.from_object(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
-@app.route("/") 
-def first(): 
-    return "<html><body> Woo I'm tipsy </body></html> "
 
-@app.route("/tasks/Susan")
+@app.before_request
+def set_up_db():
+    g.db = model.connect_db()
+
+@app.teardown_request
+def disconnect_db(exception):
+    g.db.close()
+
+@app.route("/set_date")
+def set_date():
+    session['date'] = datetime.datetime.now()
+    return "Date set"
+
+@app.route("/get_date")
+def get_date():
+    return str(session['date'])
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/")
 def index():
-    return render_template("index.html", user_name="Susan")
+    return render_template("index.html", user_name="chriszf")
+
+@app.route("/save_task", methods=["POST", "GET"])
+def save_task():
+    title = request.form['task_title']
+    model.new_task(g.db, title)
+    return redirect("/tasks")
 
 @app.route("/tasks")
 def list_tasks():
-    db= model.connect_db() 
-    tasks_from_db= model.get_tasks(db, None) 
-    return render_template("list_tasks.html", tasks= tasks_from_db)
+    user_id = session.get("user_id", None)
+    tasks_from_db = model.get_tasks(g.db, None)
+    users_from_db= model.call_users(g.db, None)
+    return render_template("list_tasks.html", tasks=tasks_from_db, 
+        users= users_from_db)
 
-@app.route("/new_task")
-def new_task_box():
-    db= model.connect_db()
-    tasks_from_db= model.get_tasks(db, None) 
-    users_from_db= model.call_users(db, None)
-    return render_template("new_task.html", tasks= tasks_from_db, 
-                            users= users_from_db)
+@app.route("/task/<int:id>", methods=["GET"])
+def view_task(id):
+    task_from_db = model.get_task(g.db, id)
+    return render_template("view_task.html", task=task_from_db)
+
+@app.route("/task/<int:id>", methods=["POST"])
+def complete_task(id):
+    model.complete_task(g.db, id)
+    return redirect("/tasks")
+
+
+@app.route("/authenticate", methods=["POST", "GET"])
+def authenticate():
+    email = request.form['email']
+    password = request.form['password']
+    user_id = model.authenticate(g.db, email, password)
+    session['user_id'] = user_id
+    return redirect("/tasks")
+
+# THis is the OLD MODEL.py here below#
+
+# @app.route("/new_task")
+# def new_task_box():
+#     tasks_from_db= model.get_tasks(g.db, None) 
+#     users_from_db= model.call_users(g.db, None)
+#     return render_template("list_tasks.html", tasks= tasks_from_db, 
+#                             users= users_from_db)
 
 @app.route("/save_task", methods=["POST"])    
 def new_task():
     task_title= request.form['task_title']
-    db = model.connect_db()
     # Assume that all tasks are attached to user 1.
-    task_id = model.new_task(db, task_title, 1)
-    return redirect("/new_task")
-
+    task_id = model.new_task(g.db, task_title, 1)
+    return redirect("/tasks")
 
 @app.route("/save_user", methods=["POST"])    
 def new_user():
     user_name= request.form['user_name']
     email= request.form['user_email']
-    db = model.connect_db()
     #new_user(db, email, password, name)
-    user_id = model.new_user(db, email, None, user_name)
-    return redirect("/new_task")
+    user_id = model.new_user(g.db, email, None, user_name)
+    return redirect("/tasks")
 
 @app.route("/del_task", methods=["POST"])    
 def del_task():
     del_title= request.form['del_title']
-    db = model.connect_db()
     # Assume that all tasks are attached to user 1.
-    task_id = model.remove_task(db, int(del_title))
-    return redirect("/new_task")
+    task_id = model.remove_task(g.db, int(del_title))
+    return redirect("/tasks")
 
 @app.route("/del_user", methods=["POST"])    
 def del_user():
     del_user= request.form['del_name']
-    db = model.connect_db()
     # Assume that all tasks are attached to user 1.
-    user_id = model.remove_user(db, int(del_user))
-    return redirect("/new_task")
+    user_id = model.remove_user(g.db, int(del_user))
+    return redirect("/tasks")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
